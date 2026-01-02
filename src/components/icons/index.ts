9f6222b28855b360e421dbe9eac72ad25ea84520
Hai Huynh Ngoc <haihuynhngoc24@gmail.com>
2026-01-03 00:04:07 +0700

    refactor: project core structure

---
+++ src/components/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as Phone } from './phone.astro';
+export { default as Play } from './play.astro';
+export { default as WhatsApp } from './whatsapp.astro';
+export { default as Checkmark } from './checkmark.astro';
+export { default as Chevron } from './chevron.astro';
+export { default as ArrowUp } from './arrow-up.astro';
+export { default as Close } from './close.astro';
+export { default as Menu } from './menu.astro';
