3f747c5af666177a1f1df5b1d615b58efa822627
Hai Huynh Ngoc <haihuynhngoc24@gmail.com>
2026-01-11 16:34:20 +0700

    feat: add new pages

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,2 +1,6 @@
 export { default as Hero } from './hero.astro';
 export { default as FinalCTA } from './final-cta.astro';
+export { default as Why } from './why.astro';
+export { default as Steps } from './steps.astro';
+export { default as TextImageHero } from './text-image-hero.astro';
+export { default as Experience } from './experience.astro';
